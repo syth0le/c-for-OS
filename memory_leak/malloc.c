7ba89f28a08da7d0c6c4ca5f4c73d7408398bd3c
syth0le <chdan565@gamil.com>
2021-04-22 20:50:20 +0300

    memory_leak task

---
+++ memory_leak/malloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char * memory_for_eaten;
+    size_t bytes = (1024*1024*100);
+    int used = 0;
+    for (;;) {
+        memory_for_eaten = (char * )malloc(bytes);
+        if (memory_for_eaten == NULL) {
+            printf("\nTotal eaten memory: %i MB\n", used / 1024);
+            exit(-1);
+        }
+        used += 100;
+        printf("USED: %d MB\n", used / 1024);
+    }
+    return 0;
+}
